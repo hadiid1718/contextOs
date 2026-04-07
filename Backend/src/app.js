e20d12463a3767fb8466fe3a2fa08985624b3e85
Hadeed Ul Hassan <hadeed.hassan189@gmail.com>
2026-04-07 23:22:25 +0500

    Implement Winston for Proper logger info

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,8 +7,12 @@ import passport from "passport";
 
 import { env } from "./config/env.js";
 import { configurePassport } from "./config/passport.js";
-import { errorHandler, notFoundHandler } from "./middleware/error.middleware.js";
+import {
+  errorHandler,
+  notFoundHandler,
+} from "./middleware/error.middleware.js";
 import { apiRouter } from "./routes/index.js";
+import logger from "./config/loggers.js";
 
 configurePassport(passport);
 
@@ -19,16 +23,30 @@ app.use(
   cors({
     origin: env.appOrigin,
     credentials: true,
-  })
+  }),
 );
-app.use(morgan("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  morgan("combined", {
+    stream: { write: (message) => logger.info(message.trim()) },
+  }),
+);
 app.use(passport.initialize());
 
+app.get("/", (req, res) => {
+  res.send("Hello From ContextOS API");
+});
 app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "ok", service: "auth-service" });
+  res
+    .status(200)
+    .json({
+      status: "ok",
+      service: "contextos-api",
+      timestamp: new Date().toISOString(),
+      uptime: process.uptime(),
+    });
 });
 
 app.use("/api/v1", apiRouter);
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,18 +1,12 @@
 import { app } from "./app.js";
 import { connectToDatabase } from "./config/db.js";
 import { env } from "./config/env.js";
+import logger from "./config/loggers.js";
 
-const startServer = async () => {
+const PORT = env.port
+
+app.listen(PORT, async()=> {
+ logger.info(`server is running on port: http://localhost:${PORT}`);
   await connectToDatabase();
+})
 
-  app.listen(env.port, () => {
-    // eslint-disable-next-line no-console
-    console.log(`Auth service listening on port ${env.port}`);
-  });
-};
-
-startServer().catch((error) => {
-  // eslint-disable-next-line no-console
-  console.error("Failed to start auth service", error);
-  process.exit(1);
-});
